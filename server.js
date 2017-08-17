b6bae23f91179f4b18160476d69d2886a79e89aa
vcordero07 <vcordero07@gmail.com>
2017-08-16 23:55:54 -0400

    wip-adding continous integration

--- server.js
+++ server.js
@@ -1,70 +1,52 @@
 
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+
+const blogPostRouter = require('./blogPostRouter');
 
 app.use(morgan('common'));
 
-BlogPosts.create('sampleTitle1', 'sampleContent1', 'sampleAuthor1', 'publishDatedate');
-BlogPosts.create('sampleTitle2', 'sampleContent2', 'sampleAuthor2', 'publishDatedate');
-BlogPosts.create('sampleTitle3', 'sampleContent3', 'sampleAuthor3', 'publishDatedate');
+app.use(express.static('public'));
 
-app.get('/blog-post', (req, res) => {
-  res.json(BlogPosts.get());
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/blog-post', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'date'];
-  for (let i=0; i<requiredFields.length; i++){
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).json(item);
-});
+app.use('/shopping-list', blogPostRouter);
 
-app.put('/blog-post/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'date', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = `Request path (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  BlogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    date: req.body.date
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
   });
-  res.status(204).end();
-});
+}
 
-app.delete('/blog-post/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog-post item \`${req.params.id}\``);
-  res.status(204).end();
-});
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-blogPost.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
